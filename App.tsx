b0fb798b133ea56b4e19290cd76d62ee719d9d26
ItaloAlmeida <italo.almeida@weareglobals.com>
2024-12-26 12:25:13 -0300

    icon

--- App.tsx
+++
@@ -1,7 +0,0 @@
-import { View } from 'react-native';
-
-export default function App(): JSX.Element {
-  return (
-    <View />
-  );
-}
---
+++ app/App.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+import { ReactElement } from 'react';
+
+export default function App(): ReactElement {
+  return (
+    <View />
+  );
+}
