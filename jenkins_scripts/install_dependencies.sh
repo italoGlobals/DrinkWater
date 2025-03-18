80ef76c1aca29256b14e149313459fbcb4c475bc
ItaloAlmeida <italo.almeida@weareglobals.com>
2025-03-18 13:53:10 -0300

    chore: update install_dependencies.sh to install Maven, Gradle, and Node.js, while streamlining environment setup and configuration

--- jenkins_scripts/install_dependencies.sh
+++ jenkins_scripts/install_dependencies.sh
@@ -14,33 +14,26 @@ curl -s "https://get.sdkman.io" | bash
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
-# Instala Java 17 e Ruby via SDKMAN
+# Instala as ferramentas via SDKMAN
 sdk install java 17.0.14-jbr
-sdk install ruby
+sdk install maven latest
+sdk install gradle latest
+sdk install nodejs 22.0.0
+sdk install ruby latest
 
-# Configura Bundler e instala dependências Ruby
-gem install bundler
-bundle install
+# Configura o ambiente para usar as versões instaladas
+sdk use java 17.0.14-jbr
+sdk use nodejs 22.0.0
+sdk use ruby latest
 
-# Instala Node.js usando NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm install 22
-nvm use 22
-
-# Instala Yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
-# Configura variáveis de ambiente e adiciona ao .bashrc para persistência
-export JAVA_HOME="$HOME/.sdkman/candidates/java/17.0.14-jbr"
+# Configura variáveis de ambiente
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
 export ANDROID_HOME="/var/jenkins_home/Android/Sdk"
 
-echo 'export JAVA_HOME="$HOME/.sdkman/candidates/java/17.0.14-jbr"' >> $HOME/.bashrc
-echo 'export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"' >> $HOME/.bashrc
-echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bashrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> $HOME/.bashrc
+# Adiciona as configurações ao .bashrc
+echo 'export SDKMAN_DIR="$HOME/.sdkman"' >> $HOME/.bashrc
+echo '[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"' >> $HOME/.bashrc
+echo 'export JAVA_HOME="$HOME/.sdkman/candidates/java/current"' >> $HOME/.bashrc
 echo 'export ANDROID_HOME="/var/jenkins_home/Android/Sdk"' >> $HOME/.bashrc
 
 . $HOME/.bashrc 
