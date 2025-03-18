54a4e0c8f0dc4d0307c87c02f42504f796609d89
ItaloAlmeida <italo.almeida@weareglobals.com>
2025-03-18 11:17:04 -0300

    chore: streamline Java installation process in install_dependencies.sh and update Jenkinsfile to reflect new execution order

--- jenkins_scripts/install_dependencies.sh
+++ jenkins_scripts/install_dependencies.sh
@@ -4,14 +4,16 @@ apt-get update && apt-get upgrade -y && apt-get install -y ruby-full build-essen
 export PATH="$PATH:$(ruby -e 'puts Gem.user_dir')/bin"
 gem install fastlane -N
 
-# Instalar Java, Node e Yarn
+# Instalar Java e configurar JAVA_HOME
 apt-get install -y openjdk-17-jdk
-export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
-echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> $HOME/.bashrc
+# Encontrar o diretório real do Java
+JAVA_PATH=$(update-alternatives --list java | grep -o '^.*java-17.*/' | head -n 1)
+export JAVA_HOME="${JAVA_PATH%/bin/}"
+echo "export JAVA_HOME=${JAVA_HOME}" >> $HOME/.bashrc
 echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> $HOME/.bashrc
 
 # Verificar instalação do Java
-ls -la $JAVA_HOME
+echo "Java instalado em: $JAVA_HOME"
 java -version
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
