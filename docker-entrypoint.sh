fc4fbc5a58d2a871d83fb2f3f2ff699eba743baf
ItaloAlmeida <italo.almeida@weareglobals.com>
2025-03-25 09:38:40 -0300

    refactor docker-entrypoint.sh to remove unnecessary echo statements, streamline the release process, and improve output directory management

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-echo "Cleaning yarn cache"  
+
 yarn cache clean
-echo "Installing dependencies"
 yarn install
 
-echo "Prebuilding expo"
 npx expo prebuild --platform android
-
-echo "Starting to create release"
 cd android/
 ./gradlew clean assembleRelease
 
 RELEASE_VERSION=$(date -Iminutes)
 
 mv ./app/build/outputs/apk/release ./app/build/outputs/apk/$RELEASE_VERSION
-
 mkdir -p /app/release_outputs
-
 cp -r ./app/build/outputs/apk release_outputs
-
 mkdir -p /app/output
 cp -r ./app/build/outputs/apk/* /app/output/
