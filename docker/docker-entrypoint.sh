175fd2fa60b356f6892cc62d29e583558db8c7bc
ItaloAlmeida <italo.almeida@weareglobals.com>
2025-03-25 13:07:25 -0300

    refactor CI workflow and docker-entrypoint.sh to streamline APK build process and output management, replacing Docker run commands with Docker Compose and simplifying output directory structure

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,8 +7,4 @@ npx expo prebuild --platform android
 cd android/
 ./gradlew clean assembleRelease
 
-RELEASE_VERSION=$(date -Iminutes)
-
-mkdir -p /app/output/$RELEASE_VERSION
-
-cp ./app/build/outputs/apk/release/app-release.apk /app/output/$RELEASE_VERSION/app-release.apk
+cp ./app/build/outputs/apk/release/app-release.apk /app/output/app-release.apk
