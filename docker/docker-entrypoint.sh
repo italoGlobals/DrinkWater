7eec02379c0f3954e68c9601204ea1c8316420d4
ItaloAlmeida <italo.almeida@weareglobals.com>
2025-03-25 16:07:12 -0300

    Optimize Docker build process by adding system prune command in CI workflow and increasing Gradle memory settings in docker-entrypoint.sh for improved performance during APK assembly.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,7 @@ yarn install
 
 npx expo prebuild --platform android
 cd android/
-./gradlew clean assembleRelease
+export GRADLE_OPTS="-Xmx8192m -XX:MaxPermSize=4096m"
+./gradlew clean assembleRelease --info --stacktrace
 
 cp ./app/build/outputs/apk/release/app-release.apk /app/output/app-release.apk
